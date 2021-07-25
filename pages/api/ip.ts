d83e8ae93abdb55c5ec309b764a96c87c6e6be4a
Gary Xie <garyxie13@live.com>
2021-07-24 23:19:52 -0400

    Update API logic to get IP

--- pages/api/ip.ts
+++ pages/api/ip.ts
@@ -2,23 +2,33 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await axios
-    .get(
-      `https://api.getgeoapi.com/v2/ip/check?api_key=${process.env.IP_API_KEY}`
-    )
-    .then((response) => {
-      console.log({ data: response.data })
-      console.log({ remoteAddress: req.socket.remoteAddress })
-      console.log({ forwardedIp: req.headers })
-      res.status(200).json({
-        responseData: response.data,
-        remoteAddress: req.socket.remoteAddress,
-        requestHeaders: req.headers,
+  const ipAddress =
+    (req.headers['x-forwarded-for'] as string)?.split(', ')[0] ||
+    req.socket.remoteAddress
+
+  if (ipAddress) {
+    await axios
+      .get(
+        `https://api.getgeoapi.com/v2/ip/${ipAddress}?api_key=${process.env.IP_API_KEY}`
+      )
+      .then((response) => {
+        console.log({ data: response.data })
+        console.log({ remoteAddress: req.socket.remoteAddress })
+        console.log({ forwardedIp: req.headers })
+        res.status(200).json({
+          responseData: response.data,
+          remoteAddress: req.socket.remoteAddress,
+          ipAddress,
+        })
       })
+      .catch((error) => {
+        console.error({ error: error.response })
+        const errorStatus = Number(error.status) || 500
+        res.status(errorStatus).json(error)
+      })
+  } else {
+    res.status(400).json({
+      message: 'Unable to retrieve request IP address.',
     })
-    .catch((error) => {
-      console.error({ error: error.response })
-      const errorStatus = Number(error.status) || 500
-      res.status(errorStatus).json(error)
-    })
+  }
 }
