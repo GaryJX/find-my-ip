341b14b55f2a55c1d3395aa79b41de5c9a23b88d
Gary Xie <garyxie13@live.com>
2021-07-24 23:45:53 -0400

    Get Client IP info on page load

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,12 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/dist/client/router'
+import { useEffect } from 'react';
+import { useRouter } from 'next/dist/client/router';
 
 const Error404Page = () => {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    router.replace('/')
-  }, [])
-  return null
-}
+    router.replace('/');
+  }, []);
+  return null;
+};
 
-export default Error404Page
+export default Error404Page;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,44 @@
-export const Home: React.FC = () => {
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+
+type PageProps = {
+  error: boolean;
+  data: any | null;
+  ipAddress: string | null;
+};
+
+export const Home: React.FC<PageProps> = ({ error, data, ipAddress }) => {
+  const errorMessage = 'Failed to retrieve information for your IP address. Please try again later.';
   return (
     <main id="main">
-      <section>Hello</section>
+      {error && <p>{errorMessage}</p>}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <section>{ipAddress}</section>
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export const getServerSideProps: GetServerSideProps<PageProps> = async (context) => {
+  try {
+    const { req } = context;
+    const ipAddress = (req.headers['x-forwarded-for'] as string)?.split(', ')[0] || req.socket.remoteAddress || null;
+    let error: boolean = false;
+    let data: any = null;
+
+    if (ipAddress) {
+      const response = await axios.get(`https://api.getgeoapi.com/v2/ip/${ipAddress}?api_key=${process.env.IP_API_KEY}`);
+      data = response.data;
+
+      console.log({ data: response.data });
+    } else {
+      error = true;
+    }
+
+    return { props: { error, data, ipAddress } };
+  } catch (error) {
+    console.error(error);
+    return { props: { error: true, data: null, ipAddress: null } };
+  }
+};
+
+export default Home;
