d28b3b345e9992c7c8bfd0ac9c859d1d81b9a9c1
Gary Xie <garyxie13@live.com>
2021-07-25 22:04:28 -0400

    Add axios cancel token

--- components/IPLookup.tsx
+++ components/IPLookup.tsx
@@ -11,7 +11,7 @@ import {
   Input,
 } from '@chakra-ui/react';
 import { GlobalContext } from 'pages';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useMemo } from 'react';
 import { useContext } from 'react';
 import { ChevronDownIcon, SearchIcon } from '@chakra-ui/icons';
@@ -19,12 +19,15 @@ import Info from './Info';
 import { FormEvent } from 'react';
 import axios from 'axios';
 import ExpandedDetail from './ExpandedDetail';
+import { PuffLoader } from 'react-spinners';
 // import Image from 'next/image';
 
 const IPLookup = () => {
   const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(true);
   const [expandDetails, setExpandDetails] = useState(false);
   const { data, setData } = useContext(GlobalContext);
+  const cancelTokenSource = useRef(axios.CancelToken.source());
   const isp = useMemo(() => {
     if (data) {
       const unformattedISP = data.asn.organisation;
@@ -79,15 +82,25 @@ const IPLookup = () => {
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (input.trim() !== '') {
+      setLoading(true);
+      cancelTokenSource.current.cancel('User re-sent API request.');
+      cancelTokenSource.current = axios.CancelToken.source();
+      console.log('@ sent');
       axios
-        .get(`/api/ip/${input}`)
+        .get(`/api/ip/${input}`, {
+          cancelToken: cancelTokenSource.current.token,
+        })
         .then((response) => {
           console.log({ response: response.data });
           setData(response.data);
+          setLoading(false);
         })
         .catch((error) => {
-          console.error({ error });
-          // TODO: Proper error handling
+          if (!axios.isCancel(error)) {
+            console.error({ error });
+            setLoading(false);
+            // TODO: Proper error handling
+          }
         });
     }
   };
@@ -134,61 +147,69 @@ const IPLookup = () => {
         width={{ base: 'calc(100% - 4rem)', md: 'auto' }}
         maxWidth={{ base: '32.5rem', md: 'initial' }}
       >
-        <Flex direction={{ base: 'column', md: 'row' }} gridRowGap="0.25rem">
-          <Info title="IP Address" details={data?.ip} />
-          <Info title="ISP" details={isp} />
-          <Info title="Location" details={location} />
-          <Info title="Time Zone" details={timeZone} />
-        </Flex>
-        {data && (
-          <Box position="relative" width="100%" height="2.5rem">
-            <Flex
-              position="absolute"
-              background="white"
-              top="0"
-              left={{ base: '-1rem', md: '-2rem' }}
-              right={{ base: '-1rem', md: '-2rem' }}
-              p={{ base: '0 1rem 1rem', md: '0 2rem 2rem' }}
-              flexDirection="column"
-              boxShadow="lg"
-              borderRadius={5}
-            >
-              <Collapse in={expandDetails}>
-                <Flex flexDirection="column" mb="2rem" gridGap="0.25rem">
-                  <ExpandedDetail title="IP Address Type" details={data.type} />
-                  <ExpandedDetail title="Latitude" details={data.location.latitude} />
-                  <ExpandedDetail title="Longitude" details={data.location.longitude} />
-                  <ExpandedDetail title="City" details={data.city.name} />
-                  <ExpandedDetail title="Region" details={data.area.name} />
-                  <ExpandedDetail
-                    title="Country"
-                    details={
-                      data.country.name ? (
-                        <Flex alignItems="center" gridGap="0.25rem">
-                          <p>{data.country.name}</p>
-                          {data.country.flag.file && (
-                            <Image src={data.country.flag.file} width={30} alt={`Flag of ${data.country.name}`} />
-                          )}
-                        </Flex>
-                      ) : null
-                    }
-                  />
-                  <ExpandedDetail title="Time Zone" details={data.time.timezone} />
-                </Flex>
-              </Collapse>
-              <Button
-                className="button"
-                isFullWidth
-                colorScheme="blue"
-                onClick={() => setExpandDetails((prev) => !prev)}
-              >
-                <Flex alignItems="center" justifyContent="center" fontWeight="bold" flexDirection="column">
-                  <p>{expandDetails ? 'Hide' : 'Show'} details</p>
-                  {/* <ChevronDownIcon /> */}
-                </Flex>
-              </Button>
+        {loading ? (
+          <Flex p="2rem 17.5rem">
+            <PuffLoader color="#4a40bf" />
+          </Flex>
+        ) : (
+          <>
+            <Flex direction={{ base: 'column', md: 'row' }} gridRowGap="0.25rem">
+              <Info title="IP Address" details={data?.ip} />
+              <Info title="ISP" details={isp} />
+              <Info title="Location" details={location} />
+              <Info title="Time Zone" details={timeZone} />
             </Flex>
-          </Box>
+            {data && (
+              <Box position="relative" width="100%" height="2.5rem">
+                <Flex
+                  position="absolute"
+                  background="white"
+                  top="0"
+                  left={{ base: '-1rem', md: '-2rem' }}
+                  right={{ base: '-1rem', md: '-2rem' }}
+                  p={{ base: '0 1rem 1rem', md: '0 2rem 2rem' }}
+                  flexDirection="column"
+                  boxShadow="lg"
+                  borderRadius={5}
+                >
+                  <Collapse in={expandDetails}>
+                    <Flex flexDirection="column" mb="2rem" gridGap="0.25rem">
+                      <ExpandedDetail title="IP Address Type" details={data.type} />
+                      <ExpandedDetail title="Latitude" details={data.location.latitude} />
+                      <ExpandedDetail title="Longitude" details={data.location.longitude} />
+                      <ExpandedDetail title="City" details={data.city.name} />
+                      <ExpandedDetail title="Region" details={data.area.name} />
+                      <ExpandedDetail
+                        title="Country"
+                        details={
+                          data.country.name ? (
+                            <Flex alignItems="center" gridGap="0.25rem">
+                              <p>{data.country.name}</p>
+                              {data.country.flag.file && (
+                                <Image src={data.country.flag.file} width={30} alt={`Flag of ${data.country.name}`} />
+                              )}
+                            </Flex>
+                          ) : null
+                        }
+                      />
+                      <ExpandedDetail title="Time Zone" details={data.time.timezone} />
+                    </Flex>
+                  </Collapse>
+                  <Button
+                    className="button"
+                    isFullWidth
+                    colorScheme="blue"
+                    onClick={() => setExpandDetails((prev) => !prev)}
+                  >
+                    <Flex alignItems="center" justifyContent="center" fontWeight="bold" flexDirection="column">
+                      <p>{expandDetails ? 'Hide' : 'Show'} details</p>
+                      {/* <ChevronDownIcon /> */}
+                    </Flex>
+                  </Button>
+                </Flex>
+              </Box>
+            )}
+          </>
         )}
       </Flex>
     </Flex>
