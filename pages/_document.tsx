8bc249eda35f1c35a75d39bba3bdf9b0bf17e7e8
Gary Xie <garyxie13@live.com>
2021-07-26 22:17:52 -0400

    Update meta description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* // TODO: Improve my meta description content (e.g. maybe match whatever I come up with for my Hero section) */}
           <meta
             name="description"
-            content="Gary Xie is a software developer and student at University of Toronto."
+            content="Find information on your IP address, including ISP, location, and time zone."
           />
         </Head>
         <body>
@@ -16,8 +15,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
