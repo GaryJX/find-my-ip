dbf42b1b3c0e149f9b46c97de779f10dabbcae70
Gary Xie <garyxie13@live.com>
2021-07-26 22:08:35 -0400

    Update document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,16 +9,20 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>Gary Xie | Software Developer</title>
-        {/* <link rel="icon" href="/logo.svg" /> */}
+        <title>Find My IP Address</title>
         <meta
           name="viewport"
           content="height=device-height, width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, viewport-fit=cover, shrink-to-fit=no"
         />
-        {/* // TODO: Add meta tags for better SEO, and maybe PWA eventually? */}
-        {/* // TODO: Add Google Analytics for my production site to see how many users visit, and from what devices? */}
         {[400, 600, 700].map((weight) => (
-          <link key={weight} rel="preload" href={`/fonts/nunito-${weight}.woff2`} as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link
+            key={weight}
+            rel="preload"
+            href={`/fonts/nunito-${weight}.woff2`}
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
         ))}
       </Head>
       <ChakraProvider theme={theme}>
